775017a2e63af930c466cbf558b8da44d53578da
manucastro93 <j.manuelcastro@hotmail.com>
2025-04-08 11:54:36 -0300

    act cors

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS', allowedOrigins));
     }
   },
   credentials: true,
