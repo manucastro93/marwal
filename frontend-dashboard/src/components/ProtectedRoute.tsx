0aa1e1e5d7c4e61e45fad11b912dee7b1578b406
manucastro93 <j.manuelcastro@hotmail.com>
2025-04-08 13:31:01 -0300

    protec

--- frontend-dashboard/src/components/ProtectedRoute.tsx
+++ frontend-dashboard/src/components/ProtectedRoute.tsx
@@ -7,11 +7,12 @@ const ProtectedRoute: Component<{ component: Component, roles?: string[] }> = (p
   const navigate = useNavigate();
 
   createEffect(() => {
+    console.log("create efect: ", isAuthenticated)
     if (!isAuthenticated) {
       navigate('/login');
     }
   });
-
+  console.log("fuera: ", isAuthenticated)
   const Component = props.component;
   return isAuthenticated ? <Component /> : null;
 };
