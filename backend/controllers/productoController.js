b3e27f1cf8b5b6dce0b8fcdcd134bc3d69966197
manucastro93 <j.manuelcastro@hotmail.com>
2025-04-08 12:26:02 -0300

    agrego la imagen en productocontroller

--- backend/controllers/productoController.js
+++ backend/controllers/productoController.js
@@ -1,4 +1,5 @@
 const { Producto } = require('../models');
+const { ImagenProducto } = require('../models');
 const { validationResult } = require('express-validator');
 
 // Función para crear un nuevo producto
@@ -81,7 +82,12 @@ exports.eliminarProducto = async (req, res) => {
 // Función para buscar todos los productos
 exports.buscarProductos = async (req, res) => {
   try {
-    const productos = await Producto.findAll();
+    const productos = await Producto.findAll({
+      include: {
+        model: ImagenProducto,
+        as: 'imagenes',
+      },
+    });
     res.status(200).json(productos);
   } catch (error) {
     res.status(500).json({ msg: 'Error al buscar productos', error });
